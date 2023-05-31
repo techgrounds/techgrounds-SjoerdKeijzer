99db5d0042831ef0f4923be37c51546cc1d47dc0
SjoerdKeijzer <129735938+SjoerdKeijzer@users.noreply.github.com>
2023-05-31 22:25:05 +0200

    python PRG-06 finishes

--- 07_Programming_beginning/PRG-06-functions.py
+++ 07_Programming_beginning/PRG-06-functions.py
@@ -4,6 +4,7 @@
 # -- Print 5 random integers with a value between 0 and 100.
 
 # Print 5 random integers between 0 and 100
+import random
 for i in range(5):
     random_number = random.randint(0, 100)
     print(random_number)
@@ -27,3 +28,25 @@ def myfunction(name):
 
 # Call myfunction with a string argument
 myfunction("Sjoerd")
+
+# ------------- Exercise 3
+# Create a new script.
+# Copy the code below into your script.
+# def avg():
+	# write your code here
+
+# you are not allowed to edit any code below here
+# x = 128
+# y = 255
+# z = avg(x,y)
+# print("The average of",x,"and",y,"is",z)
+# Write the custom function avg() so that it returns the average of the given parameters. You are not allowed to edit any code below the second comment.
+
+def avg(x, y):
+    return (x + y) / 2
+
+# you are not allowed to edit any code below here
+x = 128
+y = 255
+z = avg(x,y)
+print("The average of",x,"and",y,"is",z)
---
+++ 07_Programming_beginning/PRG-07-lists.py
@@ -0,0 +1,19 @@
+# ------------------ Exercise 1
+# Create a new script.
+# Create a variable that contains a list of five names.
+# Loop over the list using a for loop. Print every individual name in the list on a new line.
+
+
+
+
+
+
+# -------------------- Exercise 2
+# Create a new script.
+# Create a list of five integers.
+# Use a for loop to do the following for every item in the list:
+# Print the value of that item added to the value of the next item in the list. If it is the last item, add it to the value of the first item instead (since there is no next item).
+# The first result above is created by adding 9 and 80. The second result is created by adding 80 and 16, etc. The last result is created by adding 35 and 9.
+
+
+
