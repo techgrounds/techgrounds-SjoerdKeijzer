5427439b8ec6cfda2e9c0b98d9463f773a3de994
SjoerdKeijzer <129735938+SjoerdKeijzer@users.noreply.github.com>
2023-06-01 14:58:12 +0200

    PRG-08 create and exercise 1
    finished exercise 1 - start with exercise 2

---
+++ 07_Programming_beginning/PRG-08-keyvaluepairs.py
@@ -0,0 +1,29 @@
+#---------------- Exercise 1
+# Create a new script.
+# Create a dictionary with the following keys and values:
+# Key               Value
+# Firstname         Casper
+# Last name         Velzen
+# Job title         Learning coach
+# Company           Techgrounds
+#
+#Loop over the dictionary and print every key-value pair in the terminal.
+
+
+techgrounds_dict = {
+"First name": "Casper",
+"Last name": "Velzen",
+"Job title": "Learning Coach",
+"Company": "Techgrounds",
+}
+
+for key, value in techgrounds_dict.items():
+    print(f"{key} : {value}")
+
+#----------------- Exercise 2
+# Create a new script.
+# Use user input to ask for their information (first name, last name, job title, company). Store the information in a dictionary.
+# Write the information to a csv file (comma-separated values). The data should not be overwritten when you run the script multiple times.
+# 
+
+
