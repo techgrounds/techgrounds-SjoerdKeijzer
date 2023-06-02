c34e9ea9ef56f7371aebd8f272a1e8f04796e219
SjoerdKeijzer <129735938+SjoerdKeijzer@users.noreply.github.com>
2023-06-02 14:33:19 +0200

    'update rock paper schaar

---
+++ 07_Programming_beginning/PRG-09-Rock-Paper-Scissors.py
@@ -0,0 +1,25 @@
+#--------------- Rock - Paper - Scissors game -----------------------------------
+# The player plays against a computer opponent typing either a letter (rps) or an entire word (rock paper scissors) to play their move.
+# Create a function that checks whether the move is valid or not.
+# Create another function to create a computer move.
+# Create another function to check who wins the round.
+# Finally, create a function that keeps track of the score.
+# The game should be played in a predetermined number of rounds.
+
+import random
+
+# user input 
+player_input = input("Please submit your choice: ")
+
+# Create a function that checks whether the move is valid or not.
+def valid_move():
+    valid_moves = ['rock', 'paper', 'scissors', 'r', 'p', 's']
+    
+# Create another function to create a computer move with random
+def computer_move():
+    valid_moves = ['rock', 'paper', 'scissors', 'r', 'p', 's']
+    # something with random
+
+# Create another function to check who wins the round.
+
+# create a function that keeps track of the score
