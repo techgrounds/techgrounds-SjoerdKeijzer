97324ee1e6ddbb99a5aaef6b14168da08171ecbd
SjoerdKeijzer <129735938+SjoerdKeijzer@users.noreply.github.com>
2023-06-02 12:04:18 +0200

    update on PRG exercise 2
    finished exercise 2 on to Rock Paper Schaar

--- 07_Programming_beginning/PRG-08-keyvaluepairs.py
+++ 07_Programming_beginning/PRG-08-keyvaluepairs.py
@@ -9,7 +9,16 @@
 #
 #Loop over the dictionary and print every key-value pair in the terminal.
 
+techgrounds_dict = {
+"First name": "Casper",
+"Last name": "Velzen",
+"Job title": "Learning Coach",
+"Company": "Techgrounds",
+}
 
+# Looping over dict while printing every key-value pair
+for key, value in techgrounds_dict.items():
+    print(f"{key} : {value}")
 
 
 #----------------- Exercise 2
@@ -18,29 +27,33 @@
 # Write the information to a csv file (comma-separated values). The data should not be overwritten when you run the script multiple times.
 # 
 
+
 # first we do the code to get input from user
 input_first_name = input("Please state your first name: ")
 input_last_name = input("Please state your last name: ")
 input_job_title = input("What is your job title? ")
 input_company = input("What company is your employer? ")
 
-# save input from user in a custom dictionairy
+# save input from user in a custom dictionary
 custom_dict = {
-
-"firstname" : {input_first_name},  
-"lastname" : {input_last_name},
-"job_title" : {input_job_title},
-"company" : {input_company},
+    "First name": input_first_name,
+    "Last name": input_last_name,
+    "Job title": input_job_title,
+    "Company": input_company
 }
 
-# for test purposes check values in dict
-print(custom_dict)
-
-# get the input from user into a csv file
 import csv
 
-with open('exercise.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file)
+# write the content from the dictionary to a CSV file
+with open('exercise.csv', 'a', newline='') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=custom_dict.keys())
 
-content = csv_file.read()
-print(content)
+    # In the ideal world I would like to have the header also but it keeps printing each time for each rerun of the script. I could probably fix it with enough time, but for now
+    # I decide to move on with PRG-09
+    # writer.writeheader() 
+    writer.writerow(custom_dict)
+
+# Check if content is there
+with open('exercise.csv', 'r') as csv_file:
+    content = csv_file.read()
+    print(content)
